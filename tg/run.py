fd15deddd654360f935a89cad8c8aba24fe8d42e
Eralv1lle <vladimirioskevic9@gmail.com>
2026-03-21 00:14:28 +0500

    ещё правка

--- tg/run.py
+++ tg/run.py
@@ -1,11 +1,15 @@
 from aiogram import Bot, Dispatcher
+from aiogram.client.session.aiohttp import AiohttpSession
 import asyncio
 
 from config import config
 from tg.handlers import routers
 from config import config
 
-bot = Bot(token=config.BOT_TOKEN)
+proxy = "socks5://216.128.138.177:1080"
+session = AiohttpSession(proxy=proxy)
+
+bot = Bot(token=config.BOT_TOKEN, session=session)
 dp = Dispatcher()
 dp.include_routers(*routers)
 
